8b4876d06e836b8f5184dbfe546f7b6ead31b0d3
emreturgutce <emrebursalee@gmail.com>
2020-07-12 23:33:20 +0300

    Login and Sign Up routes added

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const router = require("express").Router(),
+  passport = require("../passport");
+
+router.post(
+  "/signup",
+  passport.authenticate("signup", { session: false }),
+  (req, res) => {
+    res
+      .status(201)
+      .json({ success: true, message: "User created", data: [req.user] });
+  }
+);
+
+router.post(
+  "/login",
+  passport.authenticate("login", { session: false }),
+  async (req, res, next) => {
+    res.status(200).json({
+      success: true,
+      message: "Logged in",
+      data: { user: req.user },
+    });
+  }
+);
+
+module.exports = router;
