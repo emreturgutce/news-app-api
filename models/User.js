9809963d02d88cc218c0828cc07551d75ce36e50
emreturgutce <emrebursalee@gmail.com>
2020-07-12 23:32:12 +0300

    toJSON and findByCredentials methods added

--- models/User.js
+++ models/User.js
@@ -49,6 +49,24 @@ userSchema.methods.generateAuthToken = async function () {
   return token;
 };
 
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+  delete userObject.avatar;
+
+  return userObject;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("User does not exist");
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error("Unable to login");
+  return user;
+};
+
 userSchema.pre("save", async function (next) {
   if (this.isModified("password"))
     this.password = await bcrypt.hash(this.password, 10);
@@ -60,4 +78,6 @@ userSchema.pre("deleteOne", async function (next) {
   next();
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
