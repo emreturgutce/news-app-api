6faca84aa15d592385eb864031073ca388884b3b
emreturgutce <emrebursalee@gmail.com>
2020-07-13 16:06:30 +0300

    user deletion and avatar image operations added

--- routes/me.js
+++ routes/me.js
@@ -1,5 +1,7 @@
 const router = require("express").Router(),
-  User = require("../models/User");
+  User = require("../models/User"),
+  multer = require("multer"),
+  sharp = require("sharp");
 
 router.get("/", (req, res) => {
   res.json({ success: true, message: "User fetched", data: req.user });
@@ -21,6 +23,78 @@ router.put("/", async (req, res) => {
     res
       .status(304)
       .json({ success: false, message: "User could not modified" });
+  }
+});
+
+router.delete("/", async (req, res) => {
+  const { _id } = req.user;
+  try {
+    await User.deleteOne({ _id });
+    res.status(200).json({ success: true, message: "User deleted" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "User could not deleted" });
+  }
+});
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|png|jpeg)$/))
+      return cb(new Error("File must be image"));
+    cb(undefined, true);
+  },
+});
+
+router.post("/avatar", upload.single("avatar"), async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const user = await User.findById(_id);
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    user.avatar = buffer;
+    await user.save();
+    res.status(200).json({ success: true, message: "Avatar image added" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Avatar image could not added" });
+  }
+});
+
+router.get("/avatar", async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const user = await User.findById(_id);
+    if (!user.avatar) throw new Error("Does not have avatar image");
+    const image = user.avatar;
+    res.status(200).json({
+      success: true,
+      message: "Avatar image fetched",
+      data: { image },
+    });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ success: false, message: "Avatar image could not fetched" });
+  }
+});
+
+router.delete("/avatar", async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const user = await User.findById(_id);
+    if (!user.avatar) throw new Error("Does not have avatar image");
+    user.avatar = undefined;
+    await user.save();
+    res.status(200).json({ success: true, message: "Avatar image deleted" });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ success: false, message: "Avatar image could not deleted" });
   }
 });
 
