6b4b8e1fa98b15748459c9af7256b391d3385749
emreturgutce <emrebursalee@gmail.com>
2020-07-14 18:32:29 +0300

    tokens checker added to generateAuthToken function

--- models/User.js
+++ models/User.js
@@ -40,7 +40,11 @@ const userSchema = new mongoose.Schema(
 
 userSchema.methods.generateAuthToken = async function () {
   const token = jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET);
-  this.tokens = this.tokens.concat({ token });
+  if (this.tokens) {
+    this.tokens = this.tokens.concat({ token });
+  } else {
+    this.tokens = token;
+  }
   await this.save();
   return token;
 };
