571905a5e02a90d775c8720dd7fd978df14081a7
emreturgutce <emrebursalee@gmail.com>
2020-07-17 14:52:42 +0300

    Access-control-allow-origin header

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ require("./config/database");
 const passport = require("./config/passport");
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(require("cors")());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
