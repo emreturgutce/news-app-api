1259d91e28675b2cea189882dcd74c0e657ed208
emreturgutce <emrebursalee@gmail.com>
2020-07-17 16:21:16 +0300

    access-control-allow-origin mw deleted

--- app.js
+++ app.js
@@ -5,16 +5,11 @@ require("./config/database");
 const passport = require("./config/passport");
 
 const app = express();
-app.all(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
-app.use(require("cors")());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("morgan")("dev"));
 app.use(require("helmet")());
+app.use(require("cors")());
 
 app.use("/", require("./routes"));
 app.use(
