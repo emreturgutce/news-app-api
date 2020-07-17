d09c4e3718e17469351067da07c3e6b5777e8890
emreturgutce <emrebursalee@gmail.com>
2020-07-17 14:57:07 +0300

    cors bug

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ require("./config/database");
 const passport = require("./config/passport");
 
 const app = express();
-app.use(function (req, res, next) {
+app.all(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 app.use(require("cors")());
